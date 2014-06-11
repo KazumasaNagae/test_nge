f94295d51496adc035afec2e864601adc6f89851
長江一将 <nagae_k@forii.co.jp>
2014-06-11 18:48:46 +0900

    init

---
+++ app/models/country.rb
@@ -0,0 +1,9 @@
+class Country < ActiveRecord::Base
+  def many_or_few
+    if self.population > 1000
+      'many'
+    else
+      'few'
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestNge::Application.config.secret_key_base = '4fc93c6a8ec01bd0a1f611bc4125036aead24e0b3f1af0a036513dcb48e68b3fd630d66f4d2479994fbac625f785be198916abcfd1a2aae5b0d5bdfd1f50c9a1'
